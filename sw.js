cb1684ca20dec894a48a73fd729ffb6bea61ee13
machadi76 <139841017+machadi76@users.noreply.github.com>
2026-07-26 22:23:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='roma-2026-v4';
+const CACHE='roma-2026-final-v4-1';
 const CORE=["./", "./index.html", "./walk.html", "./map.html", "./practical-verified.html", "./restaurants.html", "./assets/app-v2.css", "./assets/app-v2.js", "./assets/app-v21.css", "./assets/app-v21.js", "./assets/app-v4.css", "./assets/app-v4.js", "./assets/map-v4.js", "./assets/portable-data.js", "./assets/icons/icon-192.png", "./assets/icons/icon-512.png", "./manifest.webmanifest"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
